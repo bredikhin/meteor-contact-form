c863b64d0d2426592d5e8e9a0bdfff42340d44b8
David Ďurika <durika.d@gmail.com>
2015-07-15 15:58:32 +0200

    autoform version set to 5.3.0

--- package.js
+++ package.js
@@ -20,7 +20,7 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('aldeed:autoform@4.1.0');
+  api.use('aldeed:autoform@5.3.0');
   api.addFiles('contact-form.js');
   api.addFiles([
     'client/views/contact-form.html',
